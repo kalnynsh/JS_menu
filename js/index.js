619bfd3025e8933b7d3a4c7f59ec682cd88089d0
kalnynsh <kda869@yandex.ru>
2018-02-09 19:26:26 +0300

    Create class MenuItem.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-function Container(containerElem, containerId, containerClassName) {
+function Container(containerElem, containerClassName, containerId) {
 
     this.elem = containerElem || 'div';
     if (containerId) this.id = containerId;
@@ -15,13 +15,46 @@ Container.prototype.render = function () {
     return containerElem;
 };
 
+
+function MenuItem(itemElem, itemClass, itemData, itemHref, itemHrefLabel) {
+    Container.call(this, itemElem, itemClass);
+
+    this.data = itemData || 'menu-item'; // data-item = itemData || 'menu-item'
+    this.href = itemHref || '#';
+    this.label = itemHrefLabel || '';
+}
+
+MenuItem.prototype = Object.create(Container.prototype);
+MenuItem.prototype.constructor = MenuItem;
+
+MenuItem.prototype.render = function () {
+    var elem = document.createElement(this.elem);
+    elem.classList.add(this.className);
+    elem.dataset.item = this.data;
+
+    var a = document.createElement('a');
+    a.href = this.href;
+    a.textContent = this.label;
+
+    elem.appendChild(a);
+
+    return elem;
+};
+
+
 // Use
 window.onload = function () {
     var content = document.querySelector('.content');
-    var div = new Container('div', 'content-id', 'content-class');
-    div = div.render();
-    div.innerHTML = '<p>New dir element</p>';
+    var divContainer = new Container('div', 'content-id', 'content-class');
+    var div = divContainer.render();
+    div.innerHTML = '<h3>div element</h3>';
+
+    for (var key in divContainer) {
+        div.innerHTML += '<p>' + key + ' = ' + divContainer[key] + '</p>';
+    }
+
     console.dir(div);
+
 
 
     content.appendChild(div);
