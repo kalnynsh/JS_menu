e21a5a835df21f9ff3058446658a2d0f91351186
kalnynsh <kda869@yandex.ru>
2018-02-09 17:40:08 +0300

    Create base class Container.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 "use strict";
+
+function Container(containerElem, containerId, containerClassName) {
+
+    this.elem = containerElem || 'div';
+    if (containerId) this.id = containerId;
+    if (containerClassName) this.className = containerClassName;
+}
+
+Container.prototype.render = function () {
+    var containerElem = document.createElement(this.elem);
+    if (this.id) containerElem.id = this.id;
+    if (this.className) containerElem.classList.add(this.className);
+
+    return containerElem;
+};
+
+// Use
+window.onload = function () {
+    var content = document.querySelector('.content');
+    var div = new Container('div', 'content-id', 'content-class');
+    div = div.render();
+    div.innerHTML = '<p>New dir element</p>';
+    console.dir(div);
+
+
+    content.appendChild(div);
+};
