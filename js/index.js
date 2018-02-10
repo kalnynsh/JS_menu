c967467e6881d1397316d3536ef3711c299c3e60
kalnynsh <kda869@yandex.ru>
2018-02-10 19:50:49 +0300

    Create class Menu with 2 levels.

--- js/index.js
+++ js/index.js
@@ -44,7 +44,7 @@ MenuItem.prototype.constructor = MenuItem;
 MenuItem.prototype.render = function () {
     var elem = document.createElement(this.elem);
     elem.classList.add(this.className);
-    elem.dataset.item = this.data;
+    elem.dataset.item = this.data || '';
 
     var a = document.createElement('a');
     a.href = this.href;
@@ -55,7 +55,7 @@ MenuItem.prototype.render = function () {
     return elem;
 };
 
-
+// 1 level Menu
 function Menu(menuElem, menuClass, menuId, menuItems) {
     Container.call(this, menuElem, menuClass, menuId);
 
@@ -82,10 +82,68 @@ Menu.prototype.render = function () {
 };
 
 
+// 2 level Menu2Levels
+function Menu2Levels(menuElem, menuClass, menuId, menuItemsLevel1, menuItemsLevel2) {
+    Container.call(this, menuElem, menuClass, menuId);
+
+    this.items1 = menuItemsLevel1;
+    this.items2 = menuItemsLevel2;
+}
+
+Menu2Levels.prototype = Object.create(Container.prototype);
+Menu2Levels.prototype.constructor = Menu2Levels;
+
+Menu2Levels.prototype.render = function () {
+    var elemName = this.elem || "ul";
+    var elem = document.createElement(elemName);
+    var ul2 = document.createElement("ul");
+    var liL1;
+
+    if (this.id) elem.id = this.id;
+    if (this.className) elem.classList.add(this.className);
+
+    for (var i = 0; i < this.items1.length; i++) {
+        if (this.items1[i] instanceof MenuItem) {
+            liL1 = this.items1[i].render();
+        }
+
+        if (this.items2.length > 0) {
+            for (var j = 0; j < this.items2.length; j++) {
+                if (this.items2[j] instanceof MenuItem) {
+                    ul2.appendChild(this.items2[j].render());
+                }
+            }
+            liL1.appendChild(ul2);
+
+            elem.appendChild(liL1);
+        }
+    }
+
+    return elem;
+};
+
+
+
 // Use
 window.onload = function () {
     var content = document.querySelector('.content');
 
+    // Container test - begin
+    // var divContainer = new Container('div', 'property', 'div-id');
+    // var div = divContainer.render();
+
+    // for (var key in divContainer) {
+    //     div.innerHTML += '<p>' + key + ' = ' + divContainer[key] + '</p>';
+    // }
+    // content.appendChild(div);
+
+    // console.dir(divContainer);
+    // setTimeout(function () {
+    //     divContainer.remove();
+    // }, 2000);
+    // Container test - end
+
+    // 1 level Menu test begin
     var itemA1 = new MenuItem('li', 'menu1__item', 'home', '/', 'Главная');
     var itemA2 = new MenuItem('li', 'menu1__item', 'catalog', '/catalog', 'Каталог');
     var itemA3 = new MenuItem('li', 'menu1__item', 'about', '/about', 'О нас');
@@ -103,6 +161,7 @@ window.onload = function () {
     ]);
 
     content.appendChild(menu.render());
+
     // For MenuItems only remove first query selected element by class
     // setTimeout(function () {
     //     item2.remove();
@@ -116,21 +175,29 @@ window.onload = function () {
     var menuBreadcrumbs = new Menu('ul', 'breadcrumbs', '', itemArray);
 
     content.append(menuBreadcrumbs.render());
+    // 1 level Menu test end
 
+    // Menu2Levels test
+    var menuL1ItemsArray = [];
+    var itemL1n1 = new MenuItem('li', 'kitchen-menu__item1', 'russian', '#', 'Русская кухня');
+    menuL1ItemsArray.push(itemL1n1);
 
-    // Container test - begin
-    // var divContainer = new Container('div', 'property', 'div-id');
-    // var div = divContainer.render();
+    var menuL2ItemsArray = [];
+    var itemL2n1 = new MenuItem('li', 'kitchen-menu__item2', 'foodstuff_1', '#', 'Бефстроганов');
+    menuL2ItemsArray.push(itemL2n1);
 
-    // for (var key in divContainer) {
-    //     div.innerHTML += '<p>' + key + ' = ' + divContainer[key] + '</p>';
-    // }
-    // content.appendChild(div);
+    var itemL2n2 = new MenuItem('li', 'kitchen-menu__item2', 'foodstuff_2', '#', 'Гусь с яблоками');
+    menuL2ItemsArray.push(itemL2n2);
 
-    // console.dir(divContainer);
-    // setTimeout(function () {
-    //     divContainer.remove();
-    // }, 2000);
-    // Container test - end
+    var itemL2n3 = new MenuItem('li', 'kitchen-menu__item2', 'foodstuff_3', '#', 'Крупеник новгородский');
+    menuL2ItemsArray.push(itemL2n3);
+
+    var itemL2n4 = new MenuItem('li', 'kitchen-menu__item2', 'foodstuff_4', '#', 'Раки по-русски');
+    menuL2ItemsArray.push(itemL2n4);
+
+    var muneKitchens = new Menu2Levels(
+        'ul', 'kitchen-menu', 'kitchen-menu-id', menuL1ItemsArray, menuL2ItemsArray);
+
+    content.append(muneKitchens.render());
 
 };
